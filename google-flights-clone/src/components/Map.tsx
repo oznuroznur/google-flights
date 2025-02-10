7b264dd2903bb58917f3e5e12f8a24a4b2f7c6a5
oznuroznur <oznuroznurr26@gmail.com>
2025-02-10 17:19:32 +0300

    map title

--- google-flights-clone/src/components/Map.tsx
+++ google-flights-clone/src/components/Map.tsx
@@ -1,7 +1,17 @@
+import { Info } from "lucide-react";
 
 function Map() {
   return (
     <div className="w-full h-[350px]">
+        <div>
+            <p className="font-semibold text-xl">
+            İstanbul kalkışlı tüm uçuşlar arasında düşük fiyatlı olanları bulun
+            <Info size={24} />
+            </p>
+            <p>
+
+            </p>
+        </div>
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1329608.8511011878!2d29.155945821853024!3d40.51411163548864!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14cadbcbf424a153%3A0xacefca4d8098da74!2zxLBzdGFuYnVsIFNhYmloYSBHw7Zrw6dlbiB1bHVzbGFyYXJhc8SxIEhhdmFsaW1hbsSx!5e0!3m2!1str!2str!4v1739196738853!5m2!1str!2str"
         width="100%"
